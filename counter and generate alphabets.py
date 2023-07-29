6fb856a6490a01521861bcf665ae6ab2d6ff53c6
pradeep_kumar <84514852+pradeep-kumar-root@users.noreply.github.com>
2023-07-30 01:02:47 +0530

    Create counter and generate alphabets.py

---
+++ counter and generate alphabets.py
@@ -0,0 +1,15 @@
+from collections import Counter 
+lis = ['x','y','x','y','c','v','b','n','m','q','w','e']
+print(Counter(lis))
+
+s = ord('A')
+print(s)
+
+import string
+print(string.ascii_lowercase)
+
+x = dict(enumerate(string.ascii_lowercase,1))
+print(x)
+
+sts = list("123485475974362387")
+print(sts)
